8f0308ed6ba50d67318fed8a8a70d1d2d9e3772a
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-29 11:47:17 +0800

    Update input.tsx

--- components/Input/input.tsx
+++ components/Input/input.tsx
@@ -1,7 +1,24 @@
 const Input = ({ type = "text" }) => {
     return (
         <div>
-            <input type={type} className="" />
+            <input
+                type={type}
+                className={`
+                block
+                rounded-md
+                px-6
+                pt-6
+                pb-1
+                w-full
+                text-md
+                text-white
+                bg-neutral-700
+                appearance-none
+                focus:outline-none
+                focus:right-0
+                peer
+                `}
+            />
         </div>
     );
 }
