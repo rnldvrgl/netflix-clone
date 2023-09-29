4dca0cba72e0edda381c6b10d4e11942ad70c995
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-29 13:35:21 +0800

    Update page.tsx

--- app/Auth/page.tsx
+++ app/Auth/page.tsx
@@ -25,32 +25,31 @@ export default function Auth() {
                 </nav>
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-                        <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
-                        {variant === 'login' ? (
-                            <div className="flex flex-col gap-4">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            {variant === 'login' ? 'Sign In' : 'Sign Up'}
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            {variant === 'register' && (
                                 <Input label="Username" id="username" onChange={(e: any) => {
                                     setName(e.target.value);
                                 }} value={name} />
-                                <Input label="Email" id="email" type="email" onChange={(e: any) => {
-                                    setEmail(e.target.value);
-                                }} value={email} />
-                                <Input label="Password" id="password" onChange={(e: any) => {
-                                    setPassword(e.target.value);
-                                }} value={password} type="password" />
-                                <Button type="button">
-                                    Login
-                                </Button>
-                                <p className="text-neutral-500 mt-12">
-                                    First time using Netflix?
-                                    <span className="text-white ml-1 hover:underline cursor-pointer" onClick={toggleVariant}>
-                                        Create an account.
-                                    </span>
-                                </p>
-                            </div>
-                        ) : (
-                            <div></div>
-                        )}
-
+                            )}
+                            <Input label="Email" id="email" type="email" onChange={(e: any) => {
+                                setEmail(e.target.value);
+                            }} value={email} />
+                            <Input label="Password" id="password" onChange={(e: any) => {
+                                setPassword(e.target.value);
+                            }} value={password} type="password" />
+                            <Button type="button">
+                                Login
+                            </Button>
+                            <p className="text-neutral-500 mt-12">
+                                First time using Netflix?
+                                <span className="text-white ml-1 hover:underline cursor-pointer" onClick={toggleVariant}>
+                                    Create an account.
+                                </span>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
