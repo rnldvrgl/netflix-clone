8a68525630e193b8246e37e3ae1b29f6245295b9
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-29 14:29:19 +0800

    add prisma global

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -3,3 +3,5 @@ import { PrismaClient } from "@prisma/client";
 const client = global.prismadb || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") global.prismadb = client;
+
+export default client;
