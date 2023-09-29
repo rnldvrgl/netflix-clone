4a4199f79b7493dc58f3e0d0cd8f7045a11ced29
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-29 11:54:09 +0800

    Update input.tsx

--- components/Input/input.tsx
+++ components/Input/input.tsx
@@ -21,8 +21,27 @@ const Input = ({ type = "text", placeholder = "" }) => {
                 `}
                 placeholder={placeholder}
             />
-            <label htmlFor="email">
-
+            <label
+                htmlFor="email"
+                className={`
+                absolute
+                text-md
+                text-zinc-400
+                duration-150
+                transform
+                -translate-y-3
+                scale-75
+                top-4
+                z-10
+                origin-[0]
+                left-6
+                peer-placeholder-shown:scale-100
+                peer-placeholder-shown:translate-y-0
+                peer-focus:scale-75
+                peer-focus:-translate-y-3
+                `}
+            >
+                Email
             </label>
         </div>
     );
