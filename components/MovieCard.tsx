2a4ac8ae7ee7d7b3d92a2c600c9299915c7e1eb9
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-10-03 10:16:02 +0800

    Update MovieCard.tsx
    Co-Authored-By: daizuke123 <61997347+daizuke123@users.noreply.github.com>

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -104,7 +104,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                             <PlayIcon className="text-black w-4 lg:w-6" />
                         </div>
                         <FavoriteButton movieId={data.id} />
-                        <div className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+                        <div onClick={() => openModal(data?.id)} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
                             <ChevronDownIcon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
                         </div>
                     </div>
